f6cae0d6d05ebe8d9d748935952263174337d0a0
nari97 <narayananak12@gmail.com>
2021-08-09 16:19:25 -0400

    Uncommented pi_const

--- Models/BaseModule.py
+++ Models/BaseModule.py
@@ -6,10 +6,10 @@ class BaseModule(nn.Module):
 
     def __init__(self):
         super(BaseModule, self).__init__()
-        #self.zero_const = nn.Parameter(torch.Tensor([0]))
-        #self.zero_const.requires_grad = False
-        #self.pi_const = nn.Parameter(torch.Tensor([3.14159265358979323846]))
-        #self.pi_const.requires_grad = False
+        self.zero_const = nn.Parameter(torch.Tensor([0]))
+        self.zero_const.requires_grad = False
+        self.pi_const = nn.Parameter(torch.Tensor([3.14159265358979323846]))
+        self.pi_const.requires_grad = False
         self.epoch = 0
 
     def load_checkpoint(self, path):
