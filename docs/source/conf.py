de2253c0f29acc6af66f75576faa8eb6255aad17
nari97 <narayananak12@gmail.com>
2021-08-23 15:54:13 -0400

    changes to conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../Models/'))
 sys.path.insert(0, os.path.abspath('../../Utils/'))
 #sys.path.insert(0, os.path.abspath('../../akge/Models/'))
