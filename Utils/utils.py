fe603bd0be9c068ec2ebae4c679cc34ef1cf24c3
nari97 <narayananak12@gmail.com>
2021-08-25 17:34:45 -0400

    Removed min_

--- Utils/utils.py
+++ Utils/utils.py
@@ -5,7 +5,7 @@ def clamp_norm(input, p = 2, dim = -1, maxnorm = 1):
     norm = torch.norm(input, p = p, dim = dim, keepdim=True)
     mask = (norm<maxnorm).long()
 
-    ans = mask*input + (1-mask)*(input/torch.clamp_min_(norm, 10e-8)*maxnorm)
+    ans = mask*input + (1-mask)*(input/torch.clamp_min(norm, 10e-8)*maxnorm)
 
     return ans
 
