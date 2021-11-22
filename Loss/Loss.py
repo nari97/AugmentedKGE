b9def267d3989047e4afde95fff386e1fe3e4e66
nari-lab <nk1581@rit.edu>
2021-11-22 12:36:18 -0500

    Moved Strategy

--- Loss/Loss.py
+++ Loss/Loss.py
@@ -4,4 +4,28 @@ class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
 
+    def _get_positive_score(self, score, y):
+        # positive_score = score[:self.batch_size]
+        # positive_score = positive_score.view(-1, self.batch_size).permute(1, 0)
+
+        positive_score = score[y == 1]
+        return positive_score
+
+    def _get_negative_score(self, score, y):
+        # negative_score = score[self.batch_size:]
+        # negative_score = negative_score.view(-1, self.batch_size).permute(1, 0)
+        negative_score = score[y == -1].view(-1,self.batch_size).permute(1,0)
+        return negative_score
+
+    def forward(self, data):
+        self.lossFn(None, None)
+        score = self.model(data)
+        p_score = self._get_positive_score(score)
+        n_score = self._get_negative_score(score)
+
+
+        loss_res = self.lossFn(p_score, n_score)
+        return loss_res
+
+
     
--- Loss/MarginLoss.py
+++ Loss/MarginLoss.py
@@ -7,40 +7,16 @@ from Utils.utils import to_var
 
 class MarginLoss(Loss):
 
-    def __init__(self, adv_temperature=None, margin=6.0, use_gpu = False):
+    def __init__(self, margin=6.0):
         super(MarginLoss, self).__init__()
         self.margin = nn.Parameter(torch.Tensor([margin]))
         self.margin.requires_grad = False
         self.loss = nn.MarginRankingLoss(margin)
 
-        self.use_gpu = use_gpu
-        
-        if adv_temperature != None:
-            self.adv_temperature = nn.Parameter(torch.Tensor([adv_temperature]))
-            self.adv_temperature.requires_grad = False
-            self.adv_flag = True
-        else:
-            self.adv_flag = False
 
-    def get_weights(self, n_score):
-        return F.softmax(-n_score * self.adv_temperature, dim=-1).detach()
-
-    def forward(self, p_score, n_score):
-        #print (p_score.shape)
-        if self.adv_flag:
-            return (self.get_weights(n_score) * torch.max(p_score - n_score, -self.margin)).sum(
-                dim=-1).mean() + self.margin
-        else:
-            t = torch.ones((len(p_score), 1))
-            ones = Tensor(t)
-
-
-            if p_score.is_cuda:
-                ones = ones.cuda()
-
-            #print (p_score.device, n_score.device, ones.device)
-            return self.loss(p_score, n_score, ones)
-
-    def predict(self, p_score, n_score):
-        score = self.forward(p_score, n_score)
-        return score.cpu().data.numpy()
+    def lossFn(self, p_score, n_score):
+        t = torch.ones((len(p_score), 1))
+        ones = Tensor(t)
+        if p_score.is_cuda:
+            ones = ones.cuda()
+        return self.loss(p_score, n_score, ones)
