a0b2fca7ccf1b450cbef3943f4b3116b980f2cd2
nari-lab <nk1581@rit.edu>
2022-01-20 12:14:32 -0500

    Added example for transe

--- Examples/Example_TransE_WN18RR.py
+++ Examples/Example_TransE_WN18RR.py
@@ -2,28 +2,23 @@ import torch
 from Train.train import train
 from Train.test import test
 model_name = "transe"
-
-#Datasets numbered from 0-7
 dataset = 6
-
 corruption = "Global"
-
 parameters = {}
-parameters["nbatches"] = 225
+parameters["batch_size"] = 10000
 parameters["nr"] = 13
-parameters["lr"] = 0.7801073119264197
-parameters["wd"] = 1.4175317295064768e-09
-parameters["m"] = 0.9263060805387795
+parameters["lr"] = 0.37621418858596917
+parameters["wd"] = 1.6566904670936488e-07
+parameters["m"] = 0.9890299829607658
 parameters["trial_index"] = 1
-parameters["dim"] = 40
-#parameters["dimr"] = 30
-parameters["pnorm"] = 2
-parameters["norm"] = True
-parameters["gamma"] = 4.226494273198768
-
-train(model_name = model_name, dataset = dataset, corruption_mode = corruption, parameters = parameters, inner_norm=True)
-
-#test(model_name = model_name, dataset = dataset, corruption_mode = corruption)
-
-
+parameters["dim"] = 174
+parameters["dimr"] = 30
+parameters["dime"] = 40
+parameters["pnorm"] = 1
+parameters["gamma"] = 7.746041492315607
+parameters["bern"] = True
+parameters["inner_norm"] = True
+dataset = "transe"
+train(model_name = model_name, dataset = dataset, corruption_mode = corruption, parameters = parameters, use_gpu = True)
+test(model_name = model_name, dataset = dataset, corruption_mode = corruption, use_gpu = True)
 
